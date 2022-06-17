a873a40d0cee793b83ae349c6dc86a61047d18b2
LemmusLemmus <63229554+LemmusLemmus@users.noreply.github.com>
2022-06-17 21:04:39 +0200

    Add GlosSITarget class. Add Newtonsoft.Json and update PlayniteSDK.

---
+++ GlosSITarget.cs
@@ -0,0 +1,43 @@
+﻿using Playnite.SDK.Models;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace GlosSIIntegration
+{
+    static class GlosSITarget
+    {
+        private static readonly string TARGET_FILENAME_PREFIX = "[GI] ";
+
+        // TODO: Testing
+        /// <summary>
+        /// Creates a GlosSITarget for a game, using the default .json structure.
+        /// </summary>
+        /// <param name="playniteGame">The game to be added to GlosSI</param>
+        /// <exception cref="FileNotFoundException">If the default target json-file could not be found.</exception>
+        /// <exception cref="DirectoryNotFoundException">If the glosSITargetsPath directory could not be found.</exception>
+        public static void Create(Game playniteGame)
+        {
+            string jsonString = File.ReadAllText("DefaultTarget.json");
+            JObject jObject = (JObject) Newtonsoft.Json.JsonConvert.DeserializeObject(jsonString);
+
+            jObject.SelectToken("name").Replace(playniteGame.Name);
+            jObject.SelectToken("icon").Replace(playniteGame.Icon);
+
+            string jsonFileName = TARGET_FILENAME_PREFIX + playniteGame.GameId + ".json";
+            jsonString = jObject.ToString();
+
+            // TODO: glosSITargetsPath & INTEGRATED_TAG_ID
+            File.WriteAllText(glosSITargetsPath + jsonFileName, jsonString);
+            playniteGame.TagIds.Add(INTEGRATED_TAG_ID);
+            SaveToSteamShortcuts(jsonFileName);
+        }
+
+        private static void SaveToSteamShortcuts(string jsonFileName)
+        {
+            // TODO: glosSIConfigPath & steamShortcutsPath
+            Process glosSIConfig = Process.Start(glosSIConfigPath, $"add {jsonFileName} {steamShortcutsPath}");
+            glosSIConfig.WaitForExit();
+        }
+    }
+}
